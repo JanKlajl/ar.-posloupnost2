5c8aa97fa75ca22de4ffa057764e47cf046b88f0
majitel <majitel@GLEN>
2022-01-30 17:36:14 +0100

    Přidejte soubory projektu.

---
+++ ar. posloupnost2/ar. posloupnost2.cpp
@@ -0,0 +1,19 @@
+﻿#include <stdio.h>
+
+int main(void) {
+
+	int diference = 0;
+	int predchozi = 0;
+
+		printf("zadejte prvni clen aritmeticke posloupnosti: ");
+		scanf("%d", &predchozi);
+
+		printf("zadejte druhy clen aritmeticke posloupnosti: ");
+		scanf("%d", &diference);
+
+		printf("\n");
+	for (int x = 1; x <= 5; x++) {
+		printf("clen cislo: %d: %d\n", x, predchozi);
+		predchozi += diference;
+	}
+}
